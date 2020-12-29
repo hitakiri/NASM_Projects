c5955639c019188874b2f9398862c1dece09e5ea
Hitakiri <evander2010@yandex.ru>
2020-12-29 10:36:57 +0300

    Add debug

--- HelloWorld/hello.asm
+++ HelloWorld/hello.asm
@@ -1,9 +1,14 @@
+;%include "io64.inc"    ; for correct debugging
+
 section .data
     text db "Hello World", 10
 section .text
     global _start
+;    global CMAIN       ; for correct debugging
 
+;CMAIN:                 ; for correct debugging
 _start:
+;    mov rbp, rsp       ; for correct debugging
     mov rax, 1
     mov rdi, 1
     mov rsi, text
